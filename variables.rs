23e96fdbc677f9c4b08819bc715ac4c62ac14f58
yuridsm <yurifullstack@gmail.com>
2022-03-12 15:27:36 -0300

    Work with variables

---
+++ variables.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // This is Immutable by default
+    let somePersonAge = 35;
+
+    // This variable is mutable
+    let mut mySonAge = 3;
+
+    // Declare a constant
+    const SCORE_LIMIT: u32 = 100;
+
+    // Shadowing
+    let x = 5;
+    let x = x + 1; // x   is now 6
+}

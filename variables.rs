da7697642c41f0e1ea98ef8827824dce0d6c06aa
yuridsm <yurifullstack@gmail.com>
2022-03-12 15:56:05 -0300

    Add new print function

--- variables.rs
+++ variables.rs
@@ -1,14 +1,20 @@
 fn main() {
     // This is Immutable by default
-    let somePersonAge = 35;
+    let some_person_age = 35;
 
     // This variable is mutable
-    let mut mySonAge = 3;
+    let mut my_son_age = 3;
+    println!("{0}", my_son_age);
+    my_son_age = 90;
 
     // Declare a constant
     const SCORE_LIMIT: u32 = 100;
 
     // Shadowing
     let x = 5;
-    let x = x + 1; // x   is now 6
+    let x = x + 1; // x is now 6
+
+    println!("{0}, {1}, {2}, {3}", some_person_age, my_son_age, SCORE_LIMIT, x);
+
+    eprintln!("This is an error!!");
 }
